e2824c5054c52b7a6fbd0aad5520550354afc950
Allen-Shao <shaoyiyang312@hotmail.com>
2016-09-19 15:09:27 +0800

    lab2 initial files

---
+++ openMP/APSPtest.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "MatUtil.h"
+
+int main(int argc, char **argv)
+{
+	if(argc != 2)
+	{
+		printf("Usage: test {N}\n");
+		exit(-1);
+	}
+
+	//generate a random matrix.
+	size_t N = atoi(argv[1]);
+	int *mat = (int*)malloc(sizeof(int)*N*N);
+	GenMatrix(mat, N);
+
+	//compute the reference result.
+	int *ref = (int*)malloc(sizeof(int)*N*N);
+	memcpy(ref, mat, sizeof(int)*N*N);
+	ST_APSP(ref, N);
+
+
+	//compute your results
+	int *result = (int*)malloc(sizeof(int)*N*N);
+	memcpy(result, mat, sizeof(int)*N*N);
+	//replace by parallel algorithm
+	ST_APSP(result, N);
+
+	//compare your result with reference result
+	if(CmpArray(result, ref, N*N))
+		printf("Your result is correct.\n");
+	else
+		printf("Your result is wrong.\n");
+
+}
