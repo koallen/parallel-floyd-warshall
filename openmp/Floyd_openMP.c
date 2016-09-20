98e518162c277186865167f4b8a1206e60e5b6c3
Liu Siyuan <weifengzi2009@gmail.com>
2016-09-21 01:15:12 +0800

    add memcpy

--- openmp/Floyd_openMP.c
+++ openmp/Floyd_openMP.c
@@ -6,21 +6,20 @@
 
 void Floyd_Warshall(int* matrix, int size){
 
-
 	int i, j, k;
-
+    int *row_k = (int*)malloc(sizeof(int)*size);
 
 	for (k = 0; k < size; k++){
-		#pragma omp parallel for private(j) schedule(static)
-		for (i = 0; i < size; i++){
-			for (j = 0; j < size; j++){
-				if (matrix[i * size + k] != -1 && matrix[k * size + j] != -1){
-					int new_path = matrix[i * size + k] + matrix[k * size + j];
-					if (new_path < matrix[i * size + j] || matrix[i * size + j] == -1)
-						matrix[i*size+j] = new_path;
-				}
-			}
-		}
-	}	
-
+        memcpy(row_k, matrix + (k * size), sizeof(int)*size);
+        #pragma omp parallel for private(j) schedule(static)
+        for (i = 0; i < size; ++i) {
+            for (j = 0; j < size; ++j) {
+                if (matrix[i * size + k] != -1 && row_k[j] != -1){
+                    int new_path = matrix[i * size + k] + row_k[j];
+                    if (new_path < matrix[i * size + j] || matrix[i * size + j] == -1)
+                        matrix[i*size+j] = new_path;
+                }
+            }
+        }
+    }
 }
