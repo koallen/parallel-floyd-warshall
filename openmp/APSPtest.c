968e6f3fc1e7ad6287a8404191db3f854a91c346
koallen <weifengzi2009@gmail.com>
2016-10-05 15:16:12 +0800

    change tab to spaces

--- openmp/APSPtest.c
+++ openmp/APSPtest.c
@@ -7,43 +7,42 @@
 
 int main(int argc, char **argv)
 {
-	struct timeval tv1, tv2;
+    struct timeval tv1, tv2;
 
-	if(argc != 2)
-	{
-		printf("Usage: test {N}\n");
-		exit(-1);
-	}
+    if(argc != 2)
+    {
+        printf("Usage: test {N}\n");
+        exit(-1);
+    }
 
-	//generate a random matrix.
-	size_t N = atoi(argv[1]);
-	int *mat = (int*)malloc(sizeof(int)*N*N);
-	GenMatrix(mat, N);
+    //generate a random matrix.
+    size_t N = atoi(argv[1]);
+    int *mat = (int*)malloc(sizeof(int)*N*N);
+    GenMatrix(mat, N);
 
-	//compute the reference result.
-	int *ref = (int*)malloc(sizeof(int)*N*N);
-	memcpy(ref, mat, sizeof(int)*N*N);
-	gettimeofday(&tv1, NULL);
-	ST_APSP(ref, N);
-	gettimeofday(&tv2, NULL);
-	long sequentialtime = (tv2.tv_sec - tv1.tv_sec)*1000000 + tv2.tv_usec - tv1.tv_usec;
-	printf("Elapsed time (sequential) = %ld usecs\n", sequentialtime);
+    //compute the reference result.
+    int *ref = (int*)malloc(sizeof(int)*N*N);
+    memcpy(ref, mat, sizeof(int)*N*N);
+    gettimeofday(&tv1, NULL);
+    ST_APSP(ref, N);
+    gettimeofday(&tv2, NULL);
+    long sequentialtime = (tv2.tv_sec - tv1.tv_sec)*1000000 + tv2.tv_usec - tv1.tv_usec;
+    printf("Elapsed time (sequential) = %ld usecs\n", sequentialtime);
 
+    //compute your results
+    int *result = (int*)malloc(sizeof(int)*N*N);
+    memcpy(result, mat, sizeof(int)*N*N);
+    //replace by parallel algorithm
+    gettimeofday(&tv1, NULL);
+    Floyd_Warshall(result, N);
+    gettimeofday(&tv2, NULL);
+    long paralleltime = (tv2.tv_sec - tv1.tv_sec)*1000000 + tv2.tv_usec - tv1.tv_usec;
+    printf("Elapsed time (parallel) = %ld usecs\n", paralleltime);
 
-	//compute your results
-	int *result = (int*)malloc(sizeof(int)*N*N);
-	memcpy(result, mat, sizeof(int)*N*N);
-	//replace by parallel algorithm
-	gettimeofday(&tv1, NULL);
-	Floyd_Warshall(result, N);
-	gettimeofday(&tv2, NULL);
-	long paralleltime = (tv2.tv_sec - tv1.tv_sec)*1000000 + tv2.tv_usec - tv1.tv_usec;
-	printf("Elapsed time (parallel) = %ld usecs\n", paralleltime);
-	
-	printf("Speed up = %.3lf\n", (double)sequentialtime/paralleltime);
-	//compare your result with reference result
-	if(CmpArray(result, ref, N*N))
-		printf("Your result is correct.\n");
-	else
-		printf("Your result is wrong.\n");
+    printf("Speed up = %.3lf\n", (double)sequentialtime/paralleltime);
+    //compare your result with reference result
+    if(CmpArray(result, ref, N*N))
+        printf("Your result is correct.\n");
+    else
+        printf("Your result is wrong.\n");
 }
--- openmp/Floyd_openMP.c
+++ openmp/Floyd_openMP.c
@@ -14,10 +14,10 @@ void Floyd_Warshall(int* matrix, int size) {
         #pragma omp for schedule(static)
         for (int i = 0; i < size; ++i) {
             for (int j = 0; j < size; ++j) {
-                if (matrix[i * size + k] != -1 && row_k[j] != -1){
+                if (matrix[i * size + k] != -1 && row_k[j] != -1) {
                     int new_path = matrix[i * size + k] + row_k[j];
                     if (new_path < matrix[i * size + j] || matrix[i * size + j] == -1)
-                        matrix[i*size+j] = new_path;
+                        matrix[i * size + j] = new_path;
                 }
             }
         }
