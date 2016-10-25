85ad1c64eab91b44e7516413b3b459da1dea5e04
Liu Siyuan <weifengzi2009@gmail.com>
2016-10-26 01:09:38 +0800

    optimize global memory access for naive implementation

--- cuda/Floyd.cu
+++ cuda/Floyd.cu
@@ -6,7 +6,7 @@ void Floyd_Warshall(int *matrix, int size)
 {
     // allocate memory
     int *matrixOnGPU;
-    cudaMalloc((void **)&matrixOnGPU, sizeof(int) * size * size);
+    cudaMalloc((void **)&matrixOnGPU, sizeof(int)*size*size);
     cudaMemcpy(matrixOnGPU, matrix, sizeof(int)*size*size, cudaMemcpyHostToDevice);
 
     // dimension
@@ -14,7 +14,7 @@ void Floyd_Warshall(int *matrix, int size)
     dim3 dimBlock(TILE_WIDTH, TILE_WIDTH, 1);
 
     // run kernel
-    for(int k = 0; k < size; k ++)
+    for(int k = 0; k < size; ++k)
         run<<<dimGrid, dimBlock>>>(matrixOnGPU, size, k);
 
     // get result back
@@ -33,10 +33,13 @@ __global__ void run(int *matrix, int size, int k)
         int i0 = i * size + j;
         int i1 = i * size + k;
         int i2 = k * size + j;
-        if(matrix[i1] != -1 && matrix[i2] != -1)
+        int i0_value = matrix[i0];
+        int i1_value = matrix[i1];
+        int i2_value = matrix[i2];
+        if(i1_value != -1 && i2_value != -1)
         {
-            int sum =  (matrix[i1] + matrix[i2]);
-            if (matrix[i0] == -1 || sum < matrix[i0])
+            int sum = i1_value + i2_value;
+            if (i0_value == -1 || sum < i0_value)
                 matrix[i0] = sum;
         }
     }
