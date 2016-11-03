502371d1ccb69600c7a6db6893f4a5ada4901b4d
koallen <weifengzi2009@gmail.com>
2016-11-03 23:14:37 +0800

    fix script error, add speedup

--- cuda/profile.sh
+++ cuda/profile.sh
@@ -1,21 +1,26 @@
 #!/bin/bash
 
+mkdir -p speedup
+mkdir -p profiling
+
 # run basic
 cp makefiles/basic.make Makefile
 cp headers/debug_basic.h debug.h
 make clean && make
 for size in 1024 2048 4096 do
-    nvprof --analysis-metrics -o basic_$size.nvprof ./test $size
+    ./test $size > speedup/basic_${size}.output
+    nvprof --analysis-metrics -o profiling/basic_${size}.nvprof ./test $size
 done
 
 # run coa
 cp makefiles/coa.make Makefile
 cp headers/debug_coa.h debug.h
 for blk in 16 32 do
-    cp headers/Floyd_coa_$blk.h Floyd_coa.h
+    cp headers/Floyd_coa_${blk}.h Floyd_coa.h
     make clean && make
     for size in 1024 2048 4096 do
-        nvprof --analysis-metrics -o coa_$blk_$size.nvprof ./test $size
+        ./test $size > speedup/coa_${blk}_${size}.output
+        nvprof --analysis-metrics -o profiling/coa_${blk}_${size}.nvprof ./test $size
     done
 done
 
@@ -23,10 +28,11 @@ done
 cp makefiles/sm.make Makefile
 cp headers/debug_sm.h debug.h
 for blk in 256 512 do
-    cp headers/Floyd_sm_$blk.h Floyd_sm.h
+    cp headers/Floyd_sm_${blk}.h Floyd_sm.h
     make clean && make
     for size in 1024 2048 4096 do
-        nvprof --analysis-metrics -o sm_$blk_$size.nvprof ./test $size
+        ./test $size > speedup/sm_${blk}_${size}.output
+        nvprof --analysis-metrics -o profiling/sm_${blk}_${size}.nvprof ./test $size
     done
 done
 
@@ -34,9 +40,10 @@ done
 cp makefiles/blk.make Makefile
 cp headers/debug_blk.h debug.h
 for blk in 16 32 do
-    cp headers/Floyd_blk_$blk.h Floyd_blk.h
+    cp headers/Floyd_blk_${blk}.h Floyd_blk.h
     make clean && make
     for size in 1024 2048 4096 do
-        nvprof --analysis-metrics -o blk_$blk_$size.nvprof ./test $size
+        ./test $size > speedup/blk_${blk}_${size}
+        nvprof --analysis-metrics -o profiling/blk_${blk}_${size}.nvprof ./test $size
     done
 done
